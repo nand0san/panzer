880f5b90fc343502cd86df981be748ba3cbacecc
nand0san <hancaidolosdos@hotmail.com>
2024-09-18 20:27:54 +0200

    panzer 1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='panzer',
-    version='0.1.1',
+    version='1.0.1',
     author='nand0san',
     author_email='',
     description='REST API manager under development.',
